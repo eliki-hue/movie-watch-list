c12c15794a49851e260473b4b98b574c48a290ed
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-02-24 11:48:35 +0300

    added navbar

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
+from ensurepip import bootstrap
 from gettext import install
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 #initializing the application
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+# initializing bootstrap
+bootstrap = Bootstrap(app)
 
 from app import views
